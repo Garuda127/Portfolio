15fea1f70c8ec83b4a12a7a1a74ebcdeb8556a6c
Martin Lara <martinlarap127@gmail.com>
2025-04-25 13:27:28 -0600

    update sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://neonmint.efeele.dev",
+  site: "https://garuda127-portfolio.netlify.app",
   integrations: [
     preact(),
     icon(),
